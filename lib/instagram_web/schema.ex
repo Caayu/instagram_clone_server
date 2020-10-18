2349f97b9c819a845857e0a7c4cfefd05285f396
caayu <henrycay@outlook.com>
2020-10-18 20:11:35 -0300

    Setup for first query in GraphQL using Absinthe and Phoenix

---
+++ lib/instagram_web/schema.ex
@@ -0,0 +1,13 @@
+defmodule InstagramWeb.Schema do
+  use Absinthe.Schema
+
+  alias InstagramWeb.Resolvers
+
+  import_types __MODULE__.PostTypes
+
+  query do
+    field :photos, list_of(:photo) do
+      resolve &Resolvers.Posts.photos/3
+    end
+  end
+end
